3a43293aafce253fc9ce2a8caf822e6895d35ab0
Funniray <funniray10@gmail.com>
2021-05-24 08:58:07 +0100

    Update waterdogpe

--- src/main/java/com/funniray/wdpureperms/listeners/JoinListener.java
+++ src/main/java/com/funniray/wdpureperms/listeners/JoinListener.java
@@ -1,8 +1,25 @@
+/*
+ *    WD-PurePerms - A WaterDogPE plugin that attempts at being fully compatible with PurePerms
+ *    Copyright (C) 2021  Funniray
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    I am available for any questions/requests: funniray10@gmail.com
+ */
+
 package com.funniray.wdpureperms.listeners;
 
 import com.funniray.wdpureperms.WdPureperms;
-import dev.waterdog.event.defaults.PlayerLoginEvent;
-import dev.waterdog.utils.types.Permission;
+import dev.waterdog.waterdogpe.event.defaults.PlayerLoginEvent;
+import dev.waterdog.waterdogpe.utils.types.Permission;
 
 import java.util.Set;
 import java.util.function.Consumer;
